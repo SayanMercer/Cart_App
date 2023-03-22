9b56b24a56dabe82ec1517b64e96f1702e938282
SayanMercer <dassayan14.1998@gmail.com>
2023-03-22 22:30:29 +0530

    Cart Item added after the mistake

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ import Cart from './Cart';
 function App() {
   return (
     <div className="App">
-      <h1>Cart</h1>
+      
       <Cart/>
     </div>
   );
 }
 
 export default App;
-
--- src/Cart.js
+++ src/Cart.js
@@ -1,18 +1,17 @@
 import React from 'react';
+
 import CartItem from './CartItem';
-
 class Cart extends React.Component {
-   render () {
-     return(
-        <div className="cart">
-            <CartItem/>
-            <CartItem/>
-            <CartItem/>
-            <CartItem/>
 
-        </div>
-     );
-   }
+render (){
+    return (
+           <div className="cart">
+            <CartItem/>
+            <CartItem/>
+            <CartItem/>
+           </div>
+    );
+ }
 }
 
 export default Cart;
